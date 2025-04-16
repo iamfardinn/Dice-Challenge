1e433fa30c56373ca506ba9f3dee85b98e18c688
iamfardinn <fardindm69@gmail.com>
2025-04-16 15:13:17 +0600

    second Dice Updated

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ var randomImageSource="images/"+randomDiceImage;
 var image1=document.querySelectorAll("img")[0];
 
 image1.setAttribute("src",randomImageSource);
+
+var randomNumber2=  Math.floor(Math.random()*6)+1;
+
+var randomImageSource2="images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
